b38532a384720a87d43598c8f394e1661d104cf7
Suraj Acharya <acharya.siddhantha62@gmail.com>
2019-04-25 23:56:55 -0700

    added arrow

--- ast1-Image Slider/script.js
+++ ast1-Image Slider/script.js
@@ -4,27 +4,32 @@ var imageSlider;
 var imageWidth = 700;
 var slideLengthBreak = 700;
 var imageMaxSlideLength = 2100;
+var imageIndex = 1;
+var prev=document.getElementById('prev');
+var next = document.getElementById('next');
 
 
-
- function imageSliding(){
+ function imageSlide(){
     imageSlider =  setInterval(function(){
         if(counter === -imageMaxSlideLength){
-            
+
+            clearInterval(imageSlider);                
+            setTimeout(imageSliding,2000);
+           
             console.log(counter);
             console.log(imageMaxSlideLength);
-            imageBox.style.transition = '0.6es ease';
+           // imageBox.style.transition = '0.6s ease';
             imageBox.style.left= 2100 + 'px';
             counter = 0;
             slideLengthBreak = imageWidth ;
         }else{
             if(counter === -slideLengthBreak){
-                clearInterval(imageSlider);
-                setTimeout(imageSliding,2000);
                 slideLengthBreak = slideLengthBreak + imageWidth ;
-           
+                clearInterval(imageSlider);                
+                 setTimeout(imageSlide,2000);
+                 imageIndex++;
+                 console.log(imageIndex);           
             }
-
             counter--;
              console.log(counter);
             imageBox.style.marginLeft = counter + 'px';
@@ -34,5 +39,21 @@ var imageMaxSlideLength = 2100;
     },6);
 
  }
-imageSliding();
 
+imageSlide();
+function changeMargin() {
+    document.getElementById('image-box').style.left = 1000 + 'px';
+    console.log(document.getElementById('image-box').style.left);
+  }
+
+next.addEventListener('click',function(){
+    
+    var getindex = imageIndex;
+    console.log('test');
+    clearInterval(imageSlider);
+    console.log(counter);
+    changeMargin();
+    // imageBox.style.left = -700 + 'px';
+    // console.log(imageBox.style.left);
+
+});
